570f87ec2ba37d2c48c1d5757ab87f3293d6fa44
CoddeMaster <163339302+ray-oc@users.noreply.github.com>
2025-02-07 21:59:18 +0100

    commit

---
+++ main.js
@@ -0,0 +1,28 @@
+const game = () => {
+  const userInput = prompt("Enter rock,paper,scissors: ");
+  const computer = ["rock", "paper", "scissors"];
+  const randomIndex = Math.floor(Math.random() * 3);
+  const pc = computer[randomIndex];
+
+  if (pc === userInput) {
+    console.log("Tie Game!");
+  } else if (
+    (pc === "rock" && userInput === "paper") ||
+    (pc === "scissors" && userInput === "rock") ||
+    (pc === "paper" && userInput === "scissors")
+  ) {
+    console.log("You win the Game! Congrats.");
+  } else {
+    console.log("Computer Wins! I am sorry you little guy.");
+  }
+
+  const playAgain = prompt("Play Again?(yes/no) ");
+
+  if ((playAgain = "yes")) {
+    game();
+  } else {
+    console.log("Thanks for playing!");
+  }
+};
+
+game();
