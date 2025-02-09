df3737d5bce4237eee1ddd0b3e5d1ba00e8d034d
CoddeMaster <163339302+ray-oc@users.noreply.github.com>
2025-02-09 23:26:09 +0100

    commit

--- main.js
+++ main.js
@@ -1,28 +1,46 @@
-const game = () => {
-  const userInput = prompt("Enter rock,paper,scissors: ");
-  const computer = ["rock", "paper", "scissors"];
-  const randomIndex = Math.floor(Math.random() * 3);
-  const pc = computer[randomIndex];
+let playGame = confirm("Shall we play rock, paper or scissors? ");
 
-  if (pc === userInput) {
-    console.log("Tie Game!");
-  } else if (
-    (pc === "rock" && userInput === "paper") ||
-    (pc === "scissors" && userInput === "rock") ||
-    (pc === "paper" && userInput === "scissors")
-  ) {
-    console.log("You win the Game! Congrats.");
+if (playGame) {
+  let playerChoice = prompt("Please enter (rock,paper,scissors): ");
+  if (playerChoice) {
+    let playerOne = playerChoice.trim().toLowerCase();
+    if (
+      playerOne === "rock" ||
+      playerOne === "paper" ||
+      playerOne === "scissors"
+    ) {
+      let computerChoice = Math.floor(Math.random() * 3 + 1);
+
+      let computer =
+        computerChoice === 1
+          ? "rock"
+          : computerChoice === 2
+          ? "paper"
+          : "scissors";
+
+      let result =
+        playerOne === computer
+          ? "Tie Game"
+          : playerOne === "rock" && computer === "paper"
+          ? `PlayerOne: ${playerOne}\nComputer: ${computer} \nComputer Wins!`
+          : playerOne === "rock" && computer === "scissors"
+          ? `PlayerOne: ${playerOne}\nComputer: ${computer} \n PlayerOne Wins!`
+          : playerOne === "paper" && computer === "rock"
+          ? `PlayerOne: ${playerOne}\nComputer: ${computer} \nPlayerOne Wins!`
+          : playerOne === "paper" && computer === "scissors"
+          ? `PlayerOne: ${playerOne}\nComputer: ${computer} \nComputer Wins!`
+          : playerOne === "scissors" && computer === "rock"
+          ? `PlayerOne: ${playerOne}\nComputer: ${computer} \nComputer Wins!`
+          : `PlayerOne: ${playerOne}\nComputer: ${computer} \nPlayerOne Wins!`;
+      alert(result);
+      let playAgain = confirm("Play again?");
+      playAgain ? location.reload() : alert("Ok,thanks for playing");
+    } else {
+      alert("You didn't enter rock,paper or scissors!");
+    }
   } else {
-    console.log("Computer Wins! I am sorry you luck is trash.");
+    alert("I guess you changed your mind.Maybe next time.");
   }
-
-  const playAgain = prompt("Play Again?(yes/no) ");
-
-  if (playAgain === "yes") {
-    game();
-  } else {
-    console.log("Thanks for playing!");
-  }
-};
-
-game();
+} else {
+  alert("Ok, maybe next time.");
+}
